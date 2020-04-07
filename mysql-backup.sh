fee3e8de045f7241cc038507a1d9c8fc843c98c5
farhad <iranrenter.development@gmail.com>
2020-04-07 13:02:59 +0430

    ``

---
+++ mysql-backup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+### MySQL Server Login Info ###
+MUSER="root"
+MPASS="password"
+MHOST="localhost"
+MDB="database_name"
+MYSQL="$(which mysql)"
+MYSQLDUMP="$(which mysqldump)"
+BAK="/home/db_backup/mysql"
+BAK_MONGO="/ssdb/db_backup/mongo"
+GZIP="$(which gzip)"
+
+NOW=$(date +"%Y%m%d_%H%M")
+
+### See comments below ###
+### [ ! -d $BAK ] && mkdir -p $BAK || /bin/rm -f $BAK/* ###
+[ ! -d "$BAK" ] && mkdir -p "$BAK"
+[ ! -d "$BAK_MONGO" ] && mkdir -p "$BAK_MONGO"
+
+#DBS="$($MYSQL -u $MUSER -h $MHOST -p$MPASS -Bse 'show databases')"
+#for db in $DBS
+#do
+FILE=$BAK/db_backup_final_$NOW.gz
+
+mysqldump -u $MUSER --verbose --quick --single-transaction --password=$MPASS $MDB | $GZIP -9 > $FILE
+#done
+
